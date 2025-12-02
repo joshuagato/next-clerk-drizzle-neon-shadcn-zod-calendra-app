468ac12d53e3659d9720c61404e35bc93507580c
Joshua Gato <joshuagato37@gmail.com>
2025-12-02 21:30:51 +0000

    Added drizzle db and schema

---
+++ drizzle/schema.ts
@@ -0,0 +1,70 @@
+import { DAYS_OF_WEEK_IN_ORDER } from "@/constants";
+import { relations } from "drizzle-orm";
+import { boolean, index, integer, pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+const createdAt = timestamp('createdAt').notNull().defaultNow();
+const updatedAt = timestamp('updatedAt').notNull().defaultNow().$onUpdate(() => new Date());
+
+export const EventTable = pgTable(
+    'events',    // the name of the table in the db
+    {
+        id: uuid('id').primaryKey().defaultRandom(),
+        name: text('name').notNull(),
+        description: text('description'),
+        durationInMinutes: integer('durationInMinutes').notNull(),
+        clerkUserId: text('clerkUserId').notNull(),
+        isActive: boolean('isActive').notNull().default(true),
+        createdAt,
+        updatedAt,
+    },
+    table => ([
+        index('clerkUserIdIndex').on(table.clerkUserId) //index on clerkUserId for faster querying
+    ])
+);
+
+
+export const ScheduleTable = pgTable(
+    "schedules",
+    {
+        id: uuid('id').primaryKey().defaultRandom(),
+        timezone: text('timezone').notNull(),
+        clerkUserId: text('clerkUserId').notNull().unique(),
+        createdAt,
+        updatedAt
+    }
+)
+
+
+// Define relationships for the ScheduleTable: a schedule has many availabilities
+export const scheduleRelations = relations(ScheduleTable, ({ many }) => ({
+    availabilities: many(ScheduleAvailabilityTable),
+}))
+
+
+export const scheduleDayOfWeekEnum = pgEnum("day", DAYS_OF_WEEK_IN_ORDER);
+
+
+export const ScheduleAvailabilityTable = pgTable(
+    "scheduleAvailabilities",
+    {
+        id: uuid('id').primaryKey().defaultRandom(),
+        scheduleId: uuid('scheduleId').notNull().references(() => ScheduleTable.id, { onDelete: "cascade" }),
+        startTime: text('startTime').notNull(),
+        endTime: text('endTime').notNull(),
+        dayOfWeek: scheduleDayOfWeekEnum('dayOfWeek').notNull()
+    },
+    table => ([
+        index('scheduleIdIndex').on(table.scheduleId)
+    ])
+)
+
+// Define the reverse relation: each availability belmgs to a schedule
+export const ScheduleAvailabilityRelations = relations(
+    ScheduleAvailabilityTable,
+    ({ one }) => ({
+        schedule: one(ScheduleTable, {
+            fields: [ScheduleAvailabilityTable.scheduleId],
+            references: [ScheduleTable.id]
+        })
+    })
+)
